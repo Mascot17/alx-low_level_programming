978cae543eb78a2e6b40c1f70455d3e650e53482
Mascot17 <olalekanesho93@gmail.com>
2023-02-16 03:27:35 -0800

    size of file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - size of file
+  *
+  * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char is=%i byte\n", sizeof(char));
+	printf("size of int is=%i byte\n", sizeof(int));
+	printf("size of long int is=%i byte\n", sizeof(long int));
+	printf("size of long long int is=%i byte\n", sizeof(long long int));
+	printf("size of float is=%i byte\n", sizeof(float));
+	return (0)
+}
