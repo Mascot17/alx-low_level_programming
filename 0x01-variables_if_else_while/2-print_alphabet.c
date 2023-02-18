f7eb7fb1422d5146ff75203a0b1e4450d9fda544
Mascot17 <olalekanesho93@gmail.com>
2023-02-17 19:25:57 -0800

    alphabt print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-  * main - This is insomnia
- * 
+ * main - This is insomnia
+ *
  * Return: Always 0
 */
- int main(void)
+int main(void)
 {
 	char i;
-	for("i = 'a', i <= 'z', i++")
+	for (i = 'a', i <= 'z', i++)
 		putchar(i);
-	for("i = 'A', i <= 'Z', i++")
+	for (i = 'A', i <= 'Z', i++)
 		putchar(i);
 	putchar('\n')
 }
