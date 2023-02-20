a8a3ad24e3a9ab022be55d176e92d3c8976dac09
Mascot17 <olalekanesho93@gmail.com>
2023-02-20 08:04:30 -0800

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,14 +12,8 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (i == 'e' || i == 'q')
-		{
-		putchar(i);
-		}
-		else
-		{
-			i++;
-		}
+		if (i != 'e' && i != 'q')
+			putchar(i);
 	}
 	putchar('\n');
 	return (0);
