03d162285f6827c7e69711de38b589b5e5f26e83
Mascot17 <olalekanesho93@gmail.com>
2023-02-21 17:29:02 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char i;
+
+	for (i = '%s')
+	putchar("_putchar is %s\n", i);
+	return (0);
+}
