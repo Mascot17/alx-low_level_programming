b911ccb42f8dee8128cc8a785fb44847adf5b82c
Mascot17 <olalekanesho93@gmail.com>
2023-03-02 11:39:55 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *leet - switches some characters for others
+  *@s: input string
+  *Return: string after some characters have been switched
+  */
+
+char *leet(char *s)
+{
+
+	int i, j;
+	char a[10] = "AaEeOoTtLl";
+	char b[10] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j]
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
